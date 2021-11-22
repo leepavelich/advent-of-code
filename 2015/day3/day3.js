1189a05c4c3fd5e5e771512de344f5ec2d39cd1f
leepavelich <leepavelich@MacBook-Pro.local>
2021-11-22 10:07:09 -0700

    Day 3 solved

--- 2015/day3/day3.js
+++ 2015/day3/day3.js
@@ -19,4 +19,31 @@ const day3_1 = () => {
   console.log('Total houses visited:', Object.keys(houses).length)
 }
 
+const day3_2 = () => {
+  let houses = {house0_0: 1}
+  let [xS, yS, xR, yR] = [0, 0, 0, 0] // S = Santa, R = Robo-Santa
+
+  for (let dir = 0; dir < input.length-1; dir += 2) {
+    switch (input[dir]) {
+      case '>': xS++; break
+      case '<': xS--; break
+      case '^': yS++; break
+      case 'v': yS--; break
+    }
+
+    switch (input[dir+1]) {
+      case '>': xR++; break
+      case '<': xR--; break
+      case '^': yR++; break
+      case 'v': yR--; break
+    }
+
+    houses[`house${xS}_${yS}`]++
+    houses[`house${xR}_${yR}`]++
+  }
+
+  console.log('Total houses visited:', Object.keys(houses).length)
+}
+
 day3_1()
+day3_2()
