aff73fbccad1b2cba198974cf75cd52c5423a6a3
leepavelich <leepavelich@MacBook-Pro.local>
2021-11-22 09:39:20 -0700

    Refactoring day1 code

--- 2015/day1/day1.js
+++ 2015/day1/day1.js
@@ -2,15 +2,29 @@ const fs = require("fs");
 const input = fs.readFileSync("input").toString('utf-8').split('')
 //console.log(input)
 
-// const input = '()())'.split('')
+const day1_1 = () => {
+  let floor = 0
 
-let floor = 0
-let pos   = 0
-for (let char of input) {
-  (char === '(') ? floor++ : floor--
-  pos++
-  if (floor === -1) {console.log(`First position entering basement: ${pos}`); break}
-
+  for (let char of input) {
+    (char === '(') ? floor++ : floor--  
+  }
+  
+  console.log(`Final floor: ${floor}`)
 }
 
-console.log(`Final floor: ${floor}`)
+const day1_2 = () => {
+  let floor = 0
+  let pos   = 0
+
+  for (let char of input) {
+    (char === '(') ? floor++ : floor--
+    pos++
+    if (floor === -1) {
+      console.log(`First position entering basement: ${pos}`)
+      break
+    }
+  }
+}
+
+day1_1()
+day1_2()
