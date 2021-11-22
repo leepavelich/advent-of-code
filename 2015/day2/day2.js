015cfbe16077837cb3bce746da621e46c76ab028
leepavelich <leepavelich@MacBook-Pro.local>
2021-11-22 09:44:01 -0700

    Refactoring Day 2 code

--- 2015/day2/day2.js
+++ 2015/day2/day2.js
@@ -2,20 +2,30 @@ const fs = require("fs");
 const input = fs.readFileSync("input").toString('utf-8').split('\n')
 //console.log(input)
 
-//let input = ['4x23x21',  '22x29x19', '11x4x11',  '8x10x5']
-//let input = ['2x3x4']
+// let input = ['2x3x4']
+// let input = ['1x1x10']
 
-let sum = 0
-let inc = 1
-for (let line of input) {
-  let [l, w, h] = line.split('x').map(Number)
+const day2_1 = () => {
+  let sum = 0
 
-  //Part 1:
-  //console.log(l, w, h, Math.min(l*w,w*h,l*h))
-  //sum += 2*(l*w + w*h + h*l) + Math.min(l*w,w*h,l*h)
+  for (let line of input) {
+    let [l, w, h] = line.split('x').map(Number)
+    sum += 2*(l*w + w*h + h*l) + Math.min(l*w,w*h,l*h)
+  }
 
-  //Part2:
-  sum += 2*Math.min(l+w, l+h, w+h) + l*w*h
+  console.log(`Total surface area required: ${sum}`)
 }
 
-console.log(`Total surface area required: ${sum}`)
+const day2_2 = () => {
+  let sum = 0
+
+  for (let line of input) {
+    let [l, w, h] = line.split('x').map(Number)
+    sum += 2*Math.min(l+w, l+h, w+h) + l*w*h
+  }
+
+  console.log(`Total length required: ${sum}`)
+}
+
+day2_1()
+day2_2()
