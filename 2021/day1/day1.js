06127758910d24a5fc554541af441edf975595f9
leepavelich <leepavelich@MacBook-Pro.local>
2021-11-30 23:08:53 -0700

    Cleaned up Day 1 to return values

--- 2021/day1/day1.js
+++ 2021/day1/day1.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 const input = fs.readFileSync("input", 'utf-8').split('\n').map(l => parseInt(l))
 
-const day1_1 = () => console.log(input.map((x,i) => x > input[i-1]).filter(Boolean).length)
+const day1_1 = () => input.map((x,i) => x > input[i-1]).filter(Boolean).length
 
 const day1_2 = () => {
   let windows = input.map((x,i) => i>1 ? x+input[i-1]+input[i-2] : undefined)
-  console.log(windows.map((x,i) => x > windows[i-1]).filter(Boolean).length)
+  return windows.map((x,i) => x > windows[i-1]).filter(Boolean).length
 }
 
-day1_1()
-day1_2()
+console.log("Part 1:", day1_1(), "Part 2:", day1_2())
