14065310e05c8c629e292c14625a6a3ec1c6acb6
leepavelich <leepavelich@MacBook-Pro.local>
2021-12-07 14:03:55 -0700

    Finished Day 6

--- 2015/day6/day6.js
+++ 2015/day6/day6.js
@@ -1,32 +1,60 @@
 const fs = require("fs");
 const instructions = fs.readFileSync('input', 'utf-8').split('\n')
 
-let = instructionsTest = ['toggle 3,5 through 4,4']
+let = instructionsTest = ['turn on 0,0 through 0,0', 
+                          'toggle 0,0 through 9,9',
+                          'turn off 4,4 through 5,5',
+                          'turn off 4,4 through 5,5',
+                          'turn off 4,4 through 5,5',
+                          'turn off 4,4 through 5,5']
+let GRID_SIZE = 1000
 
-const day6 = () => {
-  let grid = gridInitialize(6)
-  for(let instruct of instructionsTest) {
+const day6 = (funcIn, funcOut) => {
+  let grid = gridInitialize(GRID_SIZE)
+  for(let instruct of instructions) {
     let digits = instruct.match(/([0-9]+).*/g)[0].split(' through ')
     let [x1,y1] = digits[0].split(',').map(x => parseInt(x))
     let [x2,y2] = digits[1].split(',').map(x => parseInt(x))
     let command = instruct.match(/(\D)+/)
-    console.log(x1,y1,x2,y2, command)
+    
+    funcIn(grid, command, [x1, y1, x2, y2])
   }
-  console.table(grid)
-  return false
+  return funcOut(grid)
 }
-
-// TODO: flatten array, apply functions to intervening values, unflatted array
 
 const gridInitialize = n => {
   grid = []
-  for(let i = 0; i < n; i++) grid.push(new Array(n).fill(false))
+  for(let i = 0; i < n; i++) grid.push(new Array(n).fill(0))
   return grid
 }
 
-const turnOn  = (grid, x, y) => grid[y][x] = true
-const turnOff = (grid, x, y) => grid[y][x] = false
-const toggle  = (grid, x, y) => grid[y][x] = !grid[y][x]
+const gridOpsBin = (grid, command, [x1, y1, x2, y2]) => {
+  for(let i = x1; i <= x2; i++) {
+    for(let j = y1; j <= y2; j++) {
+      switch(command[0]) {
+        case 'toggle '  : grid[j][i] = !grid[j][i]; break
+        case 'turn on ' : grid[j][i] = true; break
+        case 'turn off ': grid[j][i] = false; break
+      }
+    }
+  }
+}
+
+const gridOpsBright = (grid, command, [x1, y1, x2, y2]) => {
+  for(let i = x1; i <= x2; i++) {
+    for(let j = y1; j <= y2; j++) {
+      switch(command[0]) {
+        case 'toggle '  : grid[j][i] += 2; break
+        case 'turn on ' : grid[j][i]++; break
+        case 'turn off ': (grid[j][i] !== 0) ? grid[j][i]-- : grid[j][i] = 0; break
+      }
+    }
+  }
+}
+
+const lightsLit = grid => grid.flat().filter(Boolean).length
+
+const brightness = grid => grid.flat().reduce((a,b) => a + b, 0)
 
 
-console.log('Part 1', day6())
+console.log('Part 1:', day6(gridOpsBin, lightsLit), 'Part 2:', day6(gridOpsBright, brightness))
