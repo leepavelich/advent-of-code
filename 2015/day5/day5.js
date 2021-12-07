d9ba7000bb7ace4bd72e40cf59dfed5071872026
leepavelich <leepavelich@MacBook-Pro.local>
2021-12-06 21:34:47 -0700

    Day 5 Part 2 complete

--- 2015/day5/day5.js
+++ 2015/day5/day5.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync("input").toString('utf-8').split('\n')
 const day5 = (input, func) => input.filter(func).length
 
 const niceString = line => {
-  let vowels = line.match(/[aieou]/g)
+  let vowels     = line.match(/[aieou]/g)
   let disallowed = line.match(/ab|cd|pq|xy/)
   let doubles    = line.match(/([a-z])\1/)
 
@@ -14,7 +14,11 @@ const niceString = line => {
 }
 
 const newNiceString = line => {
-  return false
+  let repeat =   line.match(/(..).*\1/)
+  let sandwich = line.match(/(.).\1/)
+
+  return (repeat !== null && repeat.length > 0) &&
+         (sandwich !== null && sandwich.length > 0)
 }
 
 console.log("Part 1:", day5(input, niceString))
