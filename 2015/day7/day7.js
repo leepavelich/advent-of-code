64c5dc035e3c9149f4ae5e3c2a08d4d9d3746cb0
leepavelich <leepavelich@MacBook-Pro.local>
2021-12-07 16:08:15 -0700

    Slight edits

--- 2015/day7/day7.js
+++ 2015/day7/day7.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const gates1 = fs.readFileSync('input', 'utf-8').split('\n')
-const gates2 = fs.readFileSync('input-2', 'utf-8').split('\n')
+const gates1 = fs.readFileSync('input', 'utf-8').split('\n') // 14146 -> b
+const gates2 = fs.readFileSync('input-2', 'utf-8').split('\n') // 956 -> b
 
 
 const day7 = gates => {
-  let trans = {
+  let instructions = {
     AND:    '&',
     OR:     '|',
     NOT:    '~',
@@ -13,7 +13,7 @@ const day7 = gates => {
   }
   let sortedGates = []
   for(let gate of gates) {
-    gate = replaceAll(gate, trans)
+    gate = replaceAll(gate, instructions)
     sortedGates.push(gate.replace(/(.+?) -> (\w+)/, '$2 = $1').toUpperCase())
   }
   // sort first operand by length and then alphabetically
@@ -21,11 +21,11 @@ const day7 = gates => {
     return a.substring(0,a.indexOf('=')).length - b.substring(0,b.indexOf('=')).length || 
     a.localeCompare(b)
   })
-  eval(rotate(sortedGates,-1).join(';\n'))
+  eval(rotate(sortedGates, -1).join(';\n'))
   return A
 }
 
-const replaceAll = (str,mapObj) => {
+const replaceAll = (str, mapObj) => {
   var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
   return str.replace(re, matched => mapObj[matched])
 }
