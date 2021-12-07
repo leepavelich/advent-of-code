4fcde2e33bd12b6317d8ee2f8ac68dea2fb2c180
leepavelich <leepavelich@MacBook-Pro.local>
2021-12-06 22:35:53 -0700

    Day 7 Part 2 completed

--- 2021/day7/day7.js
+++ 2021/day7/day7.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
-const input = fs.readFileSync('input-test', 'utf-8').split(',').map(x => parseInt(x))
+const input = fs.readFileSync('input', 'utf-8').split(',').map(x => parseInt(x))
 
-const day7 = func => {
+const day7_1 = () => {
   let low  = Math.min(...input)
   let high = Math.max(...input)
 
@@ -13,6 +13,18 @@ const day7 = func => {
   return lowestFuel
 }
 
-const absDistance = (a,b) => a + Math.abs(b - i)
+const day7_2 = () => {
+  let low  = Math.min(...input)
+  let high = Math.max(...input)
 
-console.log('Part 1:', day7(absDistance))
+  let lowestFuel = Infinity
+  for(let i = low; i <= high; i++) {
+    let fuel = input.reduce((a,b) => a + triangular(Math.abs(b - i)), 0)
+    if(fuel < lowestFuel) lowestFuel = fuel
+  }
+  return lowestFuel
+}
+
+const triangular = n => (n * (n+1)) / 2
+
+console.log('Part 1:', day7_1(), 'Part 2:', day7_2())
