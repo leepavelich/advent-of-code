c83a3098c9b844244c4c8f8557d282e7eefb6bdc
leepavelich <leepavelich@MacBook-Pro.local>
2021-12-06 22:28:07 -0700

    Solved Day 7 Part 1

--- 2021/day7/day7.js
+++ 2021/day7/day7.js
@@ -1,2 +1,18 @@
 const fs = require("fs");
-const input = fs.readFileSync('input', 'utf-8').split('\n')
+const input = fs.readFileSync('input-test', 'utf-8').split(',').map(x => parseInt(x))
+
+const day7 = func => {
+  let low  = Math.min(...input)
+  let high = Math.max(...input)
+
+  let lowestFuel = Infinity
+  for(let i = low; i <= high; i++) {
+    let fuel = input.reduce((a,b) => a + Math.abs(b - i), 0)
+    if(fuel < lowestFuel) lowestFuel = fuel
+  }
+  return lowestFuel
+}
+
+const absDistance = (a,b) => a + Math.abs(b - i)
+
+console.log('Part 1:', day7(absDistance))
