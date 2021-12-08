31e3591ffe4dc4dc8c875b457d8dd47fef0cfab2
leepavelich <leepavelich@MacBook-Pro.local>
2021-12-07 18:26:20 -0700

    Added calculus solution to Day 7 as a comment

--- 2021/day7/day7.js
+++ 2021/day7/day7.js
@@ -13,6 +13,18 @@ const day7 = func => {
 const abs = n => Math.abs(n)
 const triangular = n => (Math.abs(n) * (Math.abs(n)+1)) / 2
 
+// solution from reddit using calculus df/dk = 0
+// let k = input.reduce((a,b) => a + b) / input.length
+// let kp = Math.floor(k)
+// let km = Math.ceil(k)
+// let f = [0,0,0]
+// for(let i = 0; i < input.length; i++) {
+//   f[0] += triangular(k  - input[i])
+//   f[1] += triangular(kp - input[i])
+//   f[2] += triangular(km - input[i])
+// }
+// console.table([[k, kp, km], f])
+
 const range = (from, to, step=1) => {
     return [...Array(Math.floor((to - from) / step) + 1)].map((_, i) => from + i * step)
 }
