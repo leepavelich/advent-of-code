2d2c1ed2a80194aa721abcf49b032cca36f6b263
leepavelich <leepavelich@MacBook-Pro.local>
2021-12-08 23:45:41 -0700

    Finished Part 2 with recursive explore func

--- 2021/day9/day9.js
+++ 2021/day9/day9.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-const lines = fs.readFileSync('input-test', 'utf-8').split('\n')
+const lines = fs.readFileSync('input', 'utf-8').split('\n')
 
 const day9_1 = () => {
   let sum = 0
@@ -27,4 +27,30 @@ const day9_1 = () => {
   return sum
 }
 
-console.log('Part 1:', day9_1())
+const day9_2 = () => {
+  let heightMap = new Array(lines.length)
+  for (let i = 0; i < heightMap.length; i++) {
+    heightMap[i] = lines[i].split('').map(x => parseInt(x))
+  }
+  let rows = heightMap.length
+  let cols = heightMap[0].length
+
+  const exploreBasin = (row, col) => {
+    if (row < 0 || col < 0 || row >= rows || col >= cols || heightMap[row][col] === 9) return 0
+    heightMap[row][col] = 9
+    return 1 + exploreBasin(row + 1, col) + exploreBasin(row - 1, col) 
+             + exploreBasin(row, col + 1) + exploreBasin(row, col - 1)
+  }
+
+  let basins = []
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (heightMap[row][col] !== 9) {
+        basins.push(exploreBasin(row,col))
+      }
+    }
+  }
+  return basins.sort((a,b) => b - a).slice(0,3).reduce((a,b) => a * b)
+}
+
+console.log('Part 1:', day9_1(), 'Part 2:', day9_2())
