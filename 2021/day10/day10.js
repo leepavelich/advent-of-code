35730688acb1d8341eb67275a0aa34e30187ef23
leepavelich <leepavelich@MacBook-Pro.local>
2021-12-11 20:10:09 -0700

    Combined Parts 1 and 2

--- 2021/day10/day10.js
+++ 2021/day10/day10.js
@@ -5,40 +5,27 @@ const pairs  = {'(': ')', '[': ']', '{': '}', '<': '>'}
 const scores_1 = {')': 3, ']': 57, '}': 1197, '>': 25137}
 const scores_2 = {'(': 1, '[': 2, '{': 3, '<': 4}
 
-const day10_1 = () => {
-  let sum = 0
-  for (let line of lines) {
-    let stack = []
-    for (let c of line) {
-      if (c in pairs) stack.push(c)
-      else {
-        let last = stack.pop()
-        if (pairs[last] !== c) sum += scores_1[c]
-      }
+let sum = 0
+let scores = []
+lines.forEach(line => {
+  let stack = []
+  let score = 0
+  for (let c of line) {
+    if (c in pairs) stack.push(c)
+    else {
+      let last = stack.pop()
+      if (pairs[last] !== c) {
+        sum += scores_1[c]
+          return
+        }
     }
   }
-  return sum
-}
+  for (let i = stack.length - 1; i >= 0; i--) {
+    score *= 5
+    score += scores_2[stack[i]]
+  }
+  scores.push(score)
+})
 
-const day10_2 = () => {
-  let scores = []
-  lines.forEach(line => {
-    let stack = []
-    let score = 0
-    for (let c of line) {
-      if (c in pairs) stack.push(c)
-      else {
-        let last = stack.pop()
-        if (pairs[last] !== c) return
-      }
-    }
-    for (let i = stack.length - 1; i >= 0; i--) {
-      score *= 5
-      score += scores_2[stack[i]]
-    }
-    scores.push(score)
-  })
-  return scores.sort((a,b) => a - b)[(scores.length -1) / 2]
-}
-
-console.log('Part 1:', day10_1(), 'Part 2:', day10_2())
+  console.log('Part 1:', sum)
+  console.log('Part 2:', scores.sort((a,b) => a - b)[(scores.length -1) / 2])
