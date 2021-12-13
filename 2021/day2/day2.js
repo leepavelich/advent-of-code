a5981d44312f00b3a04cd0102cb2206fa19aa441
leepavelich <leepavelich@MacBook-Pro.local>
2021-12-13 13:27:49 -0700

    Simplified Day 2 into one for loop

--- 2021/day2/day2.js
+++ 2021/day2/day2.js
@@ -1,37 +1,16 @@
 const fs = require("fs");
 const input = fs.readFileSync("input", 'utf-8').split('\n')
 
-const day2_1 = () => {
-  let x = 0
-  let y = 0
+  let [hor, dep, aim] = [0, 0, 0]
 
   for (let line of input) {
-    let commands = line.split(' ')
-    let command  = parseInt(commands[1])
-    switch(commands[0]) {
-      case 'forward': x += command; break
-      case 'down'   : y += command; break
-      case 'up'     : y -= command; break
+    let [command, n] = line.split(' ')
+    n = parseInt(n)
+    switch(command) {
+      case 'forward': hor += n; dep += aim * n; break
+      case 'down'   : aim += n; break
+      case 'up'     : aim -= n; break
     }
   }
-  return x * y
-}
 
-const day2_2 = () => {
-  let x = 0
-  let y = 0
-  let aim = 0
-
-  for (let line of input) {
-    let commands = line.split(' ')
-    let command  = parseInt(commands[1])
-    switch(commands[0]) {
-      case 'forward': x += command; y += aim * command; break
-      case 'down'   : aim += command; break
-      case 'up'     : aim -= command; break
-    }
-  }
-  return x * y
-}
-
-console.log('Part 1: ', day2_1(), 'Part 2: ', day2_2())
+  console.log('Part 1:', hor * aim, 'Part 2:', hor * dep)
