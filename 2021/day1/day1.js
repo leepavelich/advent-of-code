f533d8fb1b2886e6b131b172dcab9be8d964a8ab
leepavelich <leepavelich@MacBook-Pro.local>
2021-12-13 13:20:47 -0700

    Simplified Day 1, no need for windowing

--- 2021/day1/day1.js
+++ 2021/day1/day1.js
@@ -1,11 +1,6 @@
 const fs = require("fs");
 const input = fs.readFileSync("input", 'utf-8').split('\n').map(l => parseInt(l))
 
-const day1_1 = () => input.map((x,i) => x > input[i-1]).filter(Boolean).length
+const day1 = n => input.map((x,i) => x > input[i-n]).filter(Boolean).length
 
-const day1_2 = () => {
-  let windows = input.map((x,i) => i>1 ? x+input[i-1]+input[i-2] : undefined)
-  return windows.map((x,i) => x > windows[i-1]).filter(Boolean).length
-}
-
-console.log("Part 1:", day1_1(), "Part 2:", day1_2())
+console.log("Part 1:", day1(1), "Part 2:", day1(3))
