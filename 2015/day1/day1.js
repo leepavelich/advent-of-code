7b84bf0141b31afe758552e7a37e5510919d29aa
leepavelich <leepavelich@MacBook-Pro.local>
2022-01-23 00:33:34 -0700

    Experimenting with JS Standard

--- 2015/day1/day1.js
+++ 2015/day1/day1.js
@@ -1,22 +1,22 @@
-const fs = require("fs");
-const input = fs.readFileSync("input").toString('utf-8')
+const fs = require('fs')
+const input = fs.readFileSync('input').toString('utf-8')
 // console.log(input)
 
 const day1_1 = () => {
   let floor = 0
 
-  for (let char of input) {
-    char === '(' ? floor++ : floor--  
+  for (const char of input) {
+    char === '(' ? floor++ : floor--
   }
-  
+
   console.log(`Final floor: ${floor}`)
 }
 
 const day1_2 = () => {
   let floor = 0
-  let pos   = 0
+  let pos = 0
 
-  for (let char of input) {
+  for (const char of input) {
     char === '(' ? floor++ : floor--
     pos++
     if (floor === -1) {
