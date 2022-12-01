92a20930f3b11ce5d384261ae603508f7aa8e505
Lee Pavelich <leepavelich@gmail.com>
2022-11-30 22:16:13 -0700

    Remove example

--- common.js
+++ common.js
@@ -1,10 +1,7 @@
-export const sum = arr => arr.reduce((a, b) => a + b, 0)
+export const sum = (arr) => arr.reduce((a, b) => a + b, 0);
 
-export const prod = arr => arr.reduce((a, b) => a * b, 1)
+export const prod = (arr) => arr.reduce((a, b) => a * b, 1);
 
-export const largest = arr => arr.sort((a, b) => b - a)[0]
+export const largest = (arr) => arr.sort((a, b) => b - a)[0];
 
-export const smallest = arr => arr.sort((a, b) => a - b)[0]
-
-const ex = [1, 2, 3, 4]
-console.log(smallest(ex))
+export const smallest = (arr) => arr.sort((a, b) => a - b)[0];
