ce46dc455282ef612a2bc09f0c6939fc23f98a2f
Lee Pavelich <leepavelich@gmail.com>
2022-11-30 22:34:14 -0700

    Day 1 JS complete

--- 2022/day1/day1.js
+++ 2022/day1/day1.js
@@ -1,2 +1,23 @@
 import { readFileSync } from "fs";
-const lines = readFileSync('input-test', 'utf-8').split('\n')
+const lines = readFileSync("input", "utf-8")
+  .split("\n")
+  .map((x) => Number(x));
+
+lines.push(0);
+
+let sums = [];
+let sum = 0;
+
+for (const line of lines) {
+  if (line == 0) {
+    sums.push(sum);
+    sum = 0;
+  } else {
+    sum += line;
+  }
+}
+
+sums = sums.sort((a, b) => b - a);
+
+console.log("Largest:", sums[0]);
+console.log("Three largest:", sums[0] + sums[1] + sums[2]);
