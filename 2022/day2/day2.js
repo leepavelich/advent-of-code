02ea69f408cafd9411c96978ad57d2495c52de05
Lee Pavelich <leepavelich@gmail.com>
2022-12-01 23:07:55 -0700

    Initial solve

--- 2022/day2/day2.js
+++ 2022/day2/day2.js
@@ -1,2 +1,54 @@
 import { readFileSync } from "fs";
-const lines = readFileSync('input-test', 'utf-8').split('\n')
+const lines = readFileSync("input", "utf-8")
+  .trim()
+  .split("\n")
+  .map((l) => l.split(" "));
+
+const transpose = (matrix) => {
+  return matrix[0].map((col, i) => matrix.map((row) => row[i]));
+};
+
+const payoffs = [
+  [3, 0, 6],
+  [6, 3, 0],
+  [0, 6, 3],
+];
+
+const transposed = transpose(payoffs);
+
+const moves = {
+  A: 1,
+  B: 2,
+  C: 3,
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+
+let sum = 0;
+
+for (let line of lines) {
+  let [theirs, mine] = line;
+  sum += moves[mine] + payoffs[moves[mine] - 1][moves[theirs] - 1];
+}
+
+console.log("First sum:", sum);
+
+sum = 0;
+
+const games = {
+  A: 0,
+  B: 3,
+  C: 6,
+  X: 0,
+  Y: 3,
+  Z: 6,
+};
+
+for (let line of lines) {
+  let [theirs, condition] = line;
+  sum +=
+    games[condition] +
+    transposed[moves[theirs] - 1].indexOf(games[condition]) +
+    1;
+}
