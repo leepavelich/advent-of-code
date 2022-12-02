1e43068f790cd04cd1d637b8470a1a8bf2f820a6
Lee Pavelich <leepavelich@gmail.com>
2022-12-02 11:27:57 -0700

    Transpose directly

--- 2022/day2/day2.js
+++ 2022/day2/day2.js
@@ -7,21 +7,17 @@ const lines = readFileSync("input", "utf-8")
     l.map((c) => c.charCodeAt()).map((v) => (v < 80 ? v - 65 : v - 88))
   );
 
-const transpose = (matrix) => {
-  return matrix[0].map((col, i) => matrix.map((row) => row[i]));
-};
-
 const payoffs = [
-  [3, 0, 6],
-  [6, 3, 0],
-  [0, 6, 3],
+  [3, 6, 0],
+  [0, 3, 6],
+  [6, 0, 3],
 ];
 
-const res = [0, 3, 6];
+const res = payoffs[1];
 
-const first = (prev, [theirs, mine]) => prev + mine + payoffs[mine][theirs] + 1;
+const first = (prev, [theirs, mine]) => prev + mine + payoffs[theirs][mine] + 1;
 const second = (prev, [theirs, outcome]) =>
-  prev + res[outcome] + transpose(payoffs)[theirs].indexOf(res[outcome]) + 1;
+  prev + res[outcome] + payoffs[theirs].indexOf(res[outcome]) + 1;
 
 console.log("First sum:", lines.reduce(first, 0));
 console.log("Second sum:", lines.reduce(second, 0));
