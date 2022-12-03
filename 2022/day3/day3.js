428d3f41a02b571cc20dcd12dfa330f357468b94
Lee Pavelich <leepavelich@gmail.com>
2022-12-02 22:29:15 -0700

    Part 1 solve

--- 2022/day3/day3.js
+++ 2022/day3/day3.js
@@ -1,2 +1,32 @@
 import { readFileSync } from "fs";
-const lines = readFileSync('input-test', 'utf-8').split('\n')
+
+const priority = (c) => {
+  const v = c.charCodeAt();
+  return v > 96 ? v - 96 : v - 38;
+};
+
+const uniqueChars = (chars) =>
+  chars.filter((c, index) => {
+    return chars.indexOf(c) === index;
+  });
+
+const removeDuplicates = (string) => {
+  return uniqueChars(string.split("")).join("");
+};
+
+const lines = readFileSync("input", "utf-8")
+  .split("\n")
+  .map((l) =>
+    [l.slice(0, l.length / 2), l.slice(-l.length / 2)].map((s) =>
+      removeDuplicates(s)
+    )
+  );
+
+let sum = 0;
+
+for (let line of lines) {
+  const [first, second] = line;
+  first.split("").forEach((c) => (sum += second.includes(c) ? priority(c) : 0));
+}
+
+console.log(sum);
