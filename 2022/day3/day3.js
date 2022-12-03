83452d0a96e8eb299e29726e25a77228842cf95c
Lee Pavelich <leepavelich@gmail.com>
2022-12-02 22:47:57 -0700

    Part 2 solve

--- 2022/day3/day3.js
+++ 2022/day3/day3.js
@@ -6,27 +6,52 @@ const priority = (c) => {
 };
 
 const uniqueChars = (chars) =>
-  chars.filter((c, index) => {
-    return chars.indexOf(c) === index;
-  });
+  chars.filter((c, index) => chars.indexOf(c) === index);
 
 const removeDuplicates = (string) => {
   return uniqueChars(string.split("")).join("");
 };
 
-const lines = readFileSync("input", "utf-8")
-  .split("\n")
-  .map((l) =>
-    [l.slice(0, l.length / 2), l.slice(-l.length / 2)].map((s) =>
-      removeDuplicates(s)
-    )
-  );
+const lines = readFileSync("input", "utf-8").split("\n");
+
+let rucksacks = lines.map((l) =>
+  [l.slice(0, l.length / 2), l.slice(-l.length / 2)].map((s) =>
+    removeDuplicates(s)
+  )
+);
 
 let sum = 0;
 
-for (let line of lines) {
-  const [first, second] = line;
+for (let rucksack of rucksacks) {
+  const [first, second] = rucksack;
   first.split("").forEach((c) => (sum += second.includes(c) ? priority(c) : 0));
 }
 
-console.log(sum);
+console.log("Part 1:", sum);
+
+sum = 0;
+
+rucksacks = lines.reduce((ret, item, index) => {
+  const n = Math.floor(index / 3);
+
+  if (!ret[n]) {
+    ret[n] = []; // start a new chunk
+  }
+
+  ret[n].push(item);
+
+  return ret;
+}, []);
+
+rucksacks = rucksacks.map((s) => s.map((r) => removeDuplicates(r)));
+
+for (let rucksack of rucksacks) {
+  const [first, second, third] = rucksack;
+  first
+    .split("")
+    .forEach(
+      (c) => (sum += second.includes(c) && third.includes(c) ? priority(c) : 0)
+    );
+}
+
+console.log("Second sum:", sum);
