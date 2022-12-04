53a8f7f8a9834976a77dc8c4f248e43bcbb3a258
Lee Pavelich <leepavelich@gmail.com>
2022-12-03 22:33:47 -0700

    Day 4

--- 2022/day4/day4.js
+++ 2022/day4/day4.js
@@ -1,2 +1,19 @@
 import { readFileSync } from "fs";
-const lines = readFileSync('input-test', 'utf-8').split('\n')
+import _ from "lodash";
+
+const lines = readFileSync(process.argv.at(-1), "utf-8")
+  .trim()
+  .split("\n")
+  .map((l) =>
+    l
+      .split(",")
+      .map((a) => a.split("-").map((x) => Number(x)))
+      .map(([f, s]) => _.range(f, s + 1))
+  );
+
+const q1 = (A, B) =>
+  _.difference(A, B).length === 0 || _.difference(B, A).length === 0;
+const q2 = (A, B) => _.intersection(A, B).length > 0;
+
+console.log("Problem 1:", lines.filter(([A, B]) => q1(A, B)).length);
+console.log("Problem 2:", lines.filter(([A, B]) => q2(A, B)).length);
