395e3458330e944c37a45b95039ee762c2cdbe45
Lee Pavelich <leepavelich@gmail.com>
2022-12-04 20:44:52 -0700

    regex

--- 2022/day4/day4.js
+++ 2022/day4/day4.js
@@ -2,10 +2,10 @@ import { readFileSync } from "fs";
 const lines = readFileSync(process.argv.at(-1), "utf-8")
   .trim()
   .split("\n")
-  .map((l) => l.split(",").map((a) => a.split("-").map((x) => Number(x))));
+  .map((l) => l.split(/-|,/).map((x) => Number(x)));
 
 const q1 = (a, b, x, y) => (a <= x && b >= y) || (a >= x && b <= y);
 const q2 = (a, b, x, y) => a <= y && b >= x;
 
-console.log("P1:", lines.filter(([[a, b], [x, y]]) => q1(a, b, x, y)).length);
-console.log("P2:", lines.filter(([[a, b], [x, y]]) => q2(a, b, x, y)).length);
+console.log("P1:", lines.filter(([a, b, x, y]) => q1(a, b, x, y)).length);
+console.log("P2:", lines.filter(([a, b, x, y]) => q2(a, b, x, y)).length);
