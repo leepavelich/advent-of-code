41879cd5cf24ac772e84c2a2b842753ca0dc241b
Lee Pavelich <leepavelich@gmail.com>
2022-12-05 22:36:21 -0700

    Day 6

--- 2022/day6/day6.js
+++ 2022/day6/day6.js
@@ -1,4 +1,10 @@
 import { read } from "../../utils/js/io.js";
 import "../../utils/js/array.js";
 
-const lines = read().split("\n");
+const input = read().trim().split("");
+
+const packet = (n) =>
+  input.findIndex((e, i) => input.slice(i, i + n).uniq().length === n) + n;
+
+console.log("P1:", packet(4));
+console.log("P1:", packet(14));
