47d0cd107af74c7bb7ba55d7b5e7b993ec4a7ef6
Lee Pavelich <leepavelich@gmail.com>
2022-12-05 18:00:52 -0700

    Add toNum and largest

--- utils/js/array.js
+++ utils/js/array.js
@@ -5,3 +5,12 @@ const sum = (d) => (d ? (acc, s) => acc + d(s) : (acc, s) => acc + s);
 Array.prototype.sum = function (f) {
   return _.reduce(this, sum(f), 0);
 };
+
+Array.prototype.toNum = function (f) {
+  return this.map((x) => Number(x));
+};
+
+Array.prototype.largest = function (n) {
+  if (!n) return this.sort((a, b) => a - b).slice(-1)[0];
+  return this.sort((a, b) => a - b).slice(-n);
+};
