dcabcbe300ca4d2b7a1b70f6868a22e4c4254526
leepavelich <leepavelich@gmail.com>
2024-03-04 15:09:48 -0700

    day3/part2

--- 2015/day3/day3.py
+++ 2015/day3/day3.py
@@ -20,3 +20,40 @@ for i in instructions:
     seen[str] = 1
 
 print("Part 1:", len(seen))
+
+seen = {"0,0": 2}
+santa_loc = [0,0]
+robot_loc = [0,0]
+for idx, i in enumerate(instructions):
+  if idx % 2 == 1:
+    match i:
+      case "^":
+        robot_loc[1] += 1
+      case ">":
+        robot_loc[0] += 1
+      case "v":
+        robot_loc[1] -= 1
+      case "<":
+        robot_loc[0] -= 1
+    str_robot = f"{robot_loc[0]},{robot_loc[1]}"
+    if str_robot in seen:
+      seen[str_robot] += 1
+    else:
+      seen[str_robot] = 1
+  else:
+    match i:
+      case "^":
+        santa_loc[1] += 1
+      case ">":
+        santa_loc[0] += 1
+      case "v":
+        santa_loc[1] -= 1
+      case "<":
+        santa_loc[0] -= 1
+    str_santa = f"{santa_loc[0]},{santa_loc[1]}"
+    if str_santa in seen:
+      seen[str_santa] += 1
+    else:
+      seen[str_santa] = 1
+
+print("Part 2:", len(seen))
