3cc0dbe800c1d8ce87b8ce64d37e205c050571a9
leepavelich <leepavelich@gmail.com>
2024-03-04 14:48:40 -0700

    day2/part2

--- 2015/day2/day2.py
+++ 2015/day2/day2.py
@@ -1,5 +1,6 @@
 with open("input", "r+") as file:
   size = 0
+  ribbon = 0
   for line in file:
     l, w, h = map(int, line.strip().split('x'))
     s1 = l*w
@@ -7,4 +8,8 @@ with open("input", "r+") as file:
     s3 = l*h
     size += 2*s1 + 2*s2 + 2*s3 + min(s1,s2,s3)
 
+    srt = sorted([l, w, h])
+    ribbon += srt[0]+srt[0] + srt[1]+srt[1] + l*h*w
+
 print("Part 1:", size)
+print("Part 2:", ribbon)
