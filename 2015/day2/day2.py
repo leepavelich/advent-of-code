42a0e91d6bec8e6548e72d833b16a267ddc10159
leepavelich <leepavelich@gmail.com>
2024-03-04 14:50:36 -0700

    formatting

--- 2015/day2/day2.py
+++ 2015/day2/day2.py
@@ -9,7 +9,7 @@ with open("input", "r+") as file:
     size += 2*s1 + 2*s2 + 2*s3 + min(s1,s2,s3)
 
     srt = sorted([l, w, h])
-    ribbon += srt[0]+srt[0] + srt[1]+srt[1] + l*h*w
+    ribbon += srt[0] + srt[0] + srt[1] + srt[1] + l*h*w
 
 print("Part 1:", size)
 print("Part 2:", ribbon)
